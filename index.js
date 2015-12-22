051fb48f810aa3d9babf6b1b108cf0b2b2bca5c9
Brad Tate <brad.tate@gmail.com>
2015-12-21 21:05:48 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var config = require('./config.json');
+// var weather = require('./lib/weather.js');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
+// example event payload from slack slash command
+// token=asdfghjklzxcvbnm
+// team_id=T0001
+// team_domain=example
+// channel_id=C2147483705
+// channel_name=test
+// user_id=U2147483697
+// user_name=Steve
+// command=/weather
+// text=94070
+
+// Entrypoint for AWS Lambda
+exports.handler = function(event, context) {
+  var message = event.text ? event.text.trim() : null;
+
+  console.log('[EVENT] ', event);
+
+  // verify request came from slack - could also check that event.command === /weather
+  if(event.token !== config.slashCommandToken) {
+    return context.fail('Unauthorized request. Check config.slashCommandToken.');
+  }
+
+  var transporter = nodemailer.createTransport(smtpTransport({
+    //service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'dev6.alamode', // Your email id
+        pass: config.gmailPassword // Your password
+    }
+}));
+
+  var mailOptions = {
+      from: 'dev6.alamode@gmail.com', // sender address
+      to: 'brad.tate@gmail.com', // list of receivers
+      subject: 'Production Rollback', // Subject line
+      text: message //, // plaintext body
+      // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          console.log('[FAIL] Unable to send email: ', error);
+          context.fail('Unable to send email: ' + error);
+      }else{
+          context.succeed(info.response);
+      }
+  });
+};
